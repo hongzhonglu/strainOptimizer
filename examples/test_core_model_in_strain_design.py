228dcd1f17d345b2014744d66797fa905a7787d8
hongzhonglu <31989005+hongzhonglu@users.noreply.github.com>
2023-03-22 14:19:44 +0800

    fix: test moma

--- examples/test_core_model_in_strain_design.py
+++ examples/test_core_model_in_strain_design.py
@@ -11,11 +11,52 @@ from etfl.io.json import load_json_model, save_json_model
 from pytfa.optim.utils import symbol_sum
 from cobra.util.solver import set_objective
 import pandas as pd
+
+# function
+def prep_sol(substrate_uptake, model):
+    ret = {'obj':model.solution.objective_value,
+           'mu':model.solution.fluxes.loc[model.growth_reaction.id],
+           'available_substrate':-1*substrate_uptake,
+           'uptake':-1*model.solution.fluxes[GLC_RXN_ID]
+           }
+
+#    for exch in model.exchanges:
+#        ret[exch.id] = model.solution.fluxes.loc[exch.id]
+    for rxn in model.reactions:
+        ret[rxn.id] = model.solution.fluxes.loc[rxn.id]
+    for enz in model.enzymes:
+        ret['EZ_'+ enz.id] = model.solution.raw.loc['EZ_'+enz.id]
+    return ret #pd.Series(ret)
+
+def constrain_enzymes_based_abs_abundance(model, select_enzyme='EZ_TPI', ub0=1):
+    from etfl.optim.variables import EnzymeVariable
+    from etfl.optim.constraints import ModelConstraint
+    # a function to add a constraint on total amount of enzymes based on their
+    # fraction from total amount of proteins (should be before adding dummy)
+    enz_vars = model.get_variables_of_type(EnzymeVariable)
+    #enz_vars = [x for x in enz_vars if x.name not in exclusion]
+    single_vars = [x for x in enz_vars if x.name == select_enzyme]
+    expr = symbol_sum([x for x in single_vars])
+    model.add_constraint(kind=ModelConstraint,
+                         hook=model,
+                         expr=expr,
+                         id_='enzyme_fix_' + select_enzyme,
+                         lb=0,  # cannot be negative
+                         ub=0) # once this value is changed, it can't be replaced by the new value
+    ecoli.constraints["MODC_enzyme_fix_" + select_enzyme].ub = ub0
+    ecoli.constraints["MODC_enzyme_fix_" + select_enzyme].lb = ub0 # new
+    return model
+
+
+
 solver = 'optlang-gurobi'
 
 
 # test related to strain design method development
 ecoli = load_json_model("examples/models/ecoli/ecoli_core_curated.json", solver=solver)
+
+
+
 # definition of the initial state:
 # firstly set growth as the objective function
 ecoli.objective = "Biomass_Ecoli_core"
@@ -55,20 +96,6 @@ result.to_excel("examples/result/core_model_abundance.xlsx")
 
 # obtain the reference protein abundance
 GLC_RXN_ID ='EX_glc__D_e'
-def prep_sol(substrate_uptake, model):
-    ret = {'obj':model.solution.objective_value,
-           'mu':model.solution.fluxes.loc[model.growth_reaction.id],
-           'available_substrate':-1*substrate_uptake,
-           'uptake':-1*model.solution.fluxes[GLC_RXN_ID]
-           }
-
-#    for exch in model.exchanges:
-#        ret[exch.id] = model.solution.fluxes.loc[exch.id]
-    for rxn in model.reactions:
-        ret[rxn.id] = model.solution.fluxes.loc[rxn.id]
-    for enz in model.enzymes:
-        ret['EZ_'+ enz.id] = model.solution.raw.loc['EZ_'+enz.id]
-    return ret #pd.Series(ret)
 
 out = prep_sol(substrate_uptake=-10, model=ecoli)
 out_new = dict()
@@ -83,46 +110,18 @@ all_enzyme_ID = out_new.keys()
 
 
 
+
 # give a disturbation
-from etfl.optim.variables import EnzymeVariable
-from etfl.optim.constraints import ConstantAllocation
-from etfl.optim.constraints import ModelConstraint
-def constrain_enzymes_based_abs_abundance(model=ecoli, select_enzyme='EZ_TPI', ub0=1):
-    # a function to add a constraint on total amount of enzymes based on their
-    # fraction from total amount of proteins (should be before adding dummy)
-    enz_vars = model.get_variables_of_type(EnzymeVariable)
-    #enz_vars = [x for x in enz_vars if x.name not in exclusion]
-    single_vars = [x for x in enz_vars if x.name == select_enzyme]
-    expr = symbol_sum([x for x in single_vars])
-    model.add_constraint(kind=ModelConstraint,
-                         hook=model,
-                         expr=expr,
-                         id_='enzyme_fix_' + select_enzyme,
-                         lb=0,  # cannot be negative
-                         ub=0) # once this value is changed, it can't be replaced by the new value
-    ecoli.constraints["MODC_enzyme_fix_" + select_enzyme].ub = ub0
-    return model
-
-# test the function
-# first relax the above constraints
-# fix growth at 50% of its max value
-ecoli.reactions.get_by_id('Biomass_Ecoli_core').bounds = growth.objective_value*0.5, 3
-ecoli.reactions.get_by_id('EX_succ_e').bounds = 0, 1000
-ecoli = constrain_enzymes_based_abs_abundance(model=ecoli, select_enzyme = 'EZ_TPI', ub0=0.1)
-sol = ecoli.optimize()
-
-# retest
-ecoli.objective = 'EX_succ_e'
-ecoli.objective.direction = 'max'
-sol2 = ecoli.optimize()
-
+ecoli.reactions.get_by_id('Biomass_Ecoli_core').bounds = 0, growth.objective_value*0.5# first relax the above constraints
+ecoli.reactions.get_by_id('EX_succ_e').bounds = sol2.objective_value, 1000
+#ecoli = constrain_enzymes_based_abs_abundance(model=ecoli, select_enzyme = 'EZ_TPI', ub0=0.005)
+ecoli = constrain_enzymes_based_abs_abundance(model=ecoli, select_enzyme = 'EZ_PGI', ub0=0.00173*5)
 
 
 # run MOMA of protein resource adjust
 def symbol_sum_MOMA(variables):
     from sympy import Add
     return Add(*variables)
-
 ss = []
 for id in all_enzyme_ID:
     xx = (ecoli.enzymes.get_by_id(id).variable - out_new[id])**2
