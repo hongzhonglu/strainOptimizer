bf8af993ad22d6902b373566da1c89cead632a5c
Haoyu Wang <59432753+broke-why@users.noreply.github.com>
2024-01-05 14:02:31 +0800

    update ecFactory

--- ETFLdesigner/manipulation/constraint/total_resource_allocation.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# date : 2023/3/18 
-# author : wangh
-# file : total_resource_allocation.py
-# project : ETFLdesigner
---
+++ strainOptimizer/io/ecModel.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from geckopy.io import read_sbml_ec_model
+from cobra.io import read_sbml_model
+def load_ecmodel(filename):
+    '''load ecmodel from xml file
+    parameters:
+        filename: str, the path of the xml file
+        return:
+            model: ecmodel
+            '''
+    # model = read_sbml_ec_model(filename)
+    model=read_sbml_model(filename)
+    return model
+
+
+def ecGEM_fix_gluc(model,uptake,ex_glucID='r_1714_REV'):
+    model.reactions.get_by_id(ex_glucID).bounds=uptake,uptake
+    return model
+
+
