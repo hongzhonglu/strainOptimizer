95b40d23cdc53952333566a893ac2efffe3e5933
broke-why <wanghy@dicp.ac.cn>
2025-12-04 09:49:42 +0800

    Clear some old results.

--- examples/1.ecFactory_core_etfl_test.py
+++ examples/1.ecFactory_core_etfl_test.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-sys.path.append(r'D:\code\github\strainOptimizer')
+# sys.path.append(r'D:\code\github\strainOptimizer')
+# sys.path.append(r'D:\code\github\strainOptimizer/strainOptimizer')
 os.chdir(r'D:\code\github\strainOptimizer')
 from strainOptimizer import strainOptimizer_engine,WorkflowParameters
 
@@ -10,33 +11,32 @@ import cobra
 # cobra.Configuration().tolerance=1e-9
 
 model_params = {
-    'model_path': 'examples/models/yeast/yeast8_cEFL_2584_enz_64_bins__20231221_083715.json',
+    'model_path': 'examples/models/ecoli/ecoli_core_curated.json',
     'model_type': 'etfl',
     'solver': 'optlang-gurobi',
     # 'solver': 'optlang-cplex',
-    'growth_id': 'r_4041',
+    'growth_id': 'Biomass_Ecoli_core',
     # 'total_enzymes': 0.1
 }
 
 # Strain parameters - target product and growth conditions
 strain_params = {
-    'target_id': 'r_1589',
-    'product_name': '2-phenylethanol',
-    'c_source': 'r_1714',  # glucose exchange reaction
+    'target_id': 'EX_succ_e',
+    'product_name': 'succinate',
+    'c_source': 'EX_glc__D_e',  # glucose exchange reaction
     'c_uptake': 1,  # glucose uptake rate (mmol/gDW/h)
 }
 
 # Algorithm control parameters - workflow and output settings
 algorithm_params = {
     'design_algorithm': 'ecFactory',
-    'remove_essential': True,
+    'remove_essential': False,
     'output_directory': './results',
-    'save_results': False,
+    'save_results': True,
     'steps': 123,
     'simulation_method': 'ppfba',
-    # 'simulation_method': 'pfba',
-    # 'scanning_range':[0.1,0.4],
-    # 'experimental_yield':0.16,
+    # 'scanning_range':[0.1,0.3]
+    # 'experimental_yield':0.1,
     # 'only_final_result': True,
     # Note: ecFactory-specific parameters like steps, action_thresholds, etc.
     # would need to be added to AlgorithmControl if they're used
@@ -48,7 +48,7 @@ params = WorkflowParameters(
     strain=strain_params,
     algorithm=algorithm_params
 )
-
+parameters=params
 engine = strainOptimizer_engine(params)
 
 print(f"Engine created for {params.strain['product_name']} production")
@@ -59,10 +59,8 @@ print(f"Algorithm: {params.algorithm['design_algorithm']}")
 
 # Load model
 model = engine.load_model()
-model.tolerance=1e-7
-model.solver.configuration.timeout = 1200
-# model.solver.configuration.tolerances.optimality = 1e-4
-# model.solver.problem.Params.MIPFocus = 1
+model.tolerance=1e-9
+g = model.solver.problem
 
 # Get model information
 model_info = engine.get_model_info()
